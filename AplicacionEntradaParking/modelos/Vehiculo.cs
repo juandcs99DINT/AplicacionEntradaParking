0efaa6e1703f3a3050fd3770e246c7bc3f61d732
Juandi <Juandi@DESKTOP-LVHFGRV>
2022-01-31 19:11:21 +0100

    Agregar archivos de proyecto.

---
+++ AplicacionEntradaParking/modelos/Vehiculo.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionEntradaParking.modelos
+{
+    class Vehiculo : ObservableObject
+    {
+        public Vehiculo() { }
+
+        public Vehiculo(int idVehiculo, int idCliente, string matricula, int idMarca, string modelo, string tipo)
+        {
+            this.idVehiculo = idVehiculo;
+            this.idCliente = idCliente;
+            this.matricula = matricula;
+            this.idMarca = idMarca;
+            this.modelo = modelo;
+            this.tipo = tipo;
+        }
+
+        private int idVehiculo;
+        public int IdVehiculo
+        {
+            get => idVehiculo;
+            set => SetProperty(ref idVehiculo, value);
+        }
+
+        private int idCliente;
+        public int IdCliente
+        {
+            get => idCliente;
+            set => SetProperty(ref idCliente, value);
+        }
+
+        private string matricula;
+        public string Matricula
+        {
+            get => matricula;
+            set => SetProperty(ref matricula, value);
+        }
+
+        private int idMarca;
+        public int IdMarca
+        {
+            get => idMarca;
+            set => SetProperty(ref idMarca, value);
+        }
+
+        private string modelo;
+        public string Modelo
+        {
+            get => modelo;
+            set => SetProperty(ref modelo, value);
+        }
+
+        private string tipo;
+        public string Tipo
+        {
+            get => tipo;
+            set => SetProperty(ref tipo, value);
+        }
+    }
+}
---
+++ AplicacionEntradaParking/servicios/DatosService.cs
@@ -0,0 +1,60 @@
+﻿using AplicacionEntradaParking.modelos;
+using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicacionEntradaParking.servicios
+{
+    class DatosService
+    {
+        private readonly SqliteConnection conexion = new SqliteConnection("Data Source=" + Properties.Settings.Default.rutaConexionBd + "/parking.db");
+
+        // MÉTODOS PARA AP1
+        public int IniciarEstacionamiento(Estacionamiento estacionamiento)
+        {
+            conexion.Open();
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = "INSERT INTO estacionamientos (id_vehiculo, matricula, entrada, tipo) " +
+                "VALUES (@idVehiculo, @matricula, @entrada, @tipo)";
+            comando.Parameters.Add("@idVehiculo", SqliteType.Integer);
+            comando.Parameters.Add("@matricula", SqliteType.Text);
+            comando.Parameters.Add("@entrada", SqliteType.Text);
+            comando.Parameters.Add("@tipo", SqliteType.Text);
+            comando.Parameters["@idVehiculo"].Value = estacionamiento.IdVehiculo;
+            comando.Parameters["@matricula"].Value = estacionamiento.Matricula;
+            comando.Parameters["@entrada"].Value = estacionamiento.Entrada;
+            comando.Parameters["@tipo"].Value = estacionamiento.Tipo;
+            int filasAfectadas = comando.ExecuteNonQuery();
+            conexion.Close();
+            return filasAfectadas;
+        }
+
+        // Para controlar que un mismo vehículo no tenga dos estacionamientos activos al mismo tiempo.
+        public Estacionamiento GetEstacionamientoByMatricula(string matricula)
+        {
+            conexion.Open();
+            Estacionamiento estacionamiento = null;
+            SqliteCommand comando = conexion.CreateCommand();
+            comando.CommandText = "SELECT * FROM estacionamientos WHERE matricula = @matricula";
+            SqliteDataReader cursorEstacionamientos = comando.ExecuteReader();
+
+            comando.Parameters.Add("@matricula", SqliteType.Text);
+            comando.Parameters["@matricula"].Value = matricula;
+
+            if (cursorEstacionamientos.HasRows)
+            {
+                cursorEstacionamientos.Read();
+                estacionamiento = new Estacionamiento(cursorEstacionamientos.GetInt32(0), cursorEstacionamientos.GetInt32(1), (string)cursorEstacionamientos["matricula"],
+                        (string)cursorEstacionamientos["entrada"], (string)cursorEstacionamientos["salida"], cursorEstacionamientos.GetFloat(5), (string)cursorEstacionamientos["tipo"]);
+            }
+            cursorEstacionamientos.Close();
+            conexion.Close();
+            return estacionamiento;
+        }
+
+    }
+}
